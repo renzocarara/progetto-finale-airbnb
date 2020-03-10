14f9d13d667548669febac13240aabca799c7b51
Renzo Carara <renzo.carara@libero.it>
2020-03-10 13:51:56 +0100

    aggiunti campi 'lastname, 'date_of_birth': - sulla sulla pagina di registrazione(.blade) - nella migration per tabella 'users' - nel model users.php (campi fillabili) - nel controller Register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name'); // obbligarorio
+            $table->string('lastname')->nullable();
+            $table->string('date_of_birth')->nullable();
+            $table->string('email')->unique(); // obbligarorio
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
