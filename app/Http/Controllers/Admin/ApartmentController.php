42e361ecc1d26de2fa547da3a59e43012fd0e771
Renzo Carara <renzo.carara@libero.it>
2020-03-24 20:27:14 +0100

    sistemati commenti

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -35,23 +35,25 @@ class ApartmentController extends Controller
         // leggo dal DB tutti gli appartamenti associati all'utente loggato e ottengo una collection
         $apartments = Apartment::where('user_id', $user_id)->withTrashed()->get();
 
-        $active_sponsorships = []; // contiene gli id degli apt con sponsorizzazione
-
-        $all_sponsorships=[]; // contiene tutte le sponsorizzazioni di tutti gli appartamenti
-
+        // contiene tutte le sponsorizzazioni di tutti gli appartamenti di quell'utente (sia scadute che attive)
+        $all_sponsorships=[];
         foreach ($apartments as $apartment) {
             array_push($all_sponsorships, DB::table('apartment_sponsorship')->where('apartment_id', $apartment->id)->get());
         }
 
+        // contiene gli id degli apt con associata una sponsorizzazione attiva
+        $active_sponsorships = [];
+        // scorro la lista di tutte le sponsorizzazioni per gli appartamenti dell'utente e mi ricavo solo quelle attive
         for ($i=0; $i < count($all_sponsorships); $i++) {
             for ($j=0; $j < count($all_sponsorships[$i]); $j++) {
+                // leggo la data di inizio sponsorizzazione
                 $start_date = $all_sponsorships[$i][$j]->{'start_date'};
-
+                // la trasformo in un oggettoo Carbon
                 $start_date = Carbon::create($start_date);
-
+                // leggo il tipo della sponsorizzazione
                 $type = $all_sponsorships[$i][$j]->{'sponsorship_id'};
 
-                // duarta della sponsorizzazione
+                // calcolo la durata della sponsorizzazione in 'numero di giorni'
                 $num_of_days = ($type == 3 ? 6 : $type);
 
                 // if ($type ==3) {
@@ -60,18 +62,19 @@ class ApartmentController extends Controller
                 //     $num_of_days = $type;
                 // }
 
-                // calcolare la end date sommando alla start date la durata del tipo di sponsorizzazione
+                // calcolare la end date sommando alla start_date la durata del tipo di sponsorizzazione
                 $end_date = $start_date->addDay($num_of_days);
 
+                // verifico se la sponsorizzazione è ancora attiva
                 if ($end_date > now('Europe/Rome')) {
-
+                    // la sponsorizzazione è attiva, la metto in elenco nell'array, pushando l'id dell'appartamento
                     array_push($active_sponsorships, $all_sponsorships[$i][$j]->{'apartment_id'});
                 }
-
             }
         }
 
         // ritorno la view che visualizzerà una pagina con l'elenco degli appartamenti dell'utente loggato
+        // gli passo anche l'elenco delle sponsorizzazioni attive
         return view('admin.index', ['apartments' => $apartments, 'active_sponsorships' =>$active_sponsorships]);
     }
 
