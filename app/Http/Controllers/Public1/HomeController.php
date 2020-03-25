319a4b0f6b0cc1d4cd6b75e8b6de2bc5ae8df4c2
salvatorelomonaco <lommetto95@gmail.com>
2020-03-25 07:31:13 -0400

    Visualizzazione degli apt sponsorozzati nella view home.blade

--- app/Http/Controllers/Public1/HomeController.php
+++ app/Http/Controllers/Public1/HomeController.php
@@ -8,8 +8,10 @@ use App\Apartment;
 use App\Service;
 use App\Info;
 use App\Message;
+use App\Sponsorship;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 // use Illuminate\Support\Facades\Session; 
 
 
@@ -20,11 +22,43 @@ class HomeController extends Controller
     public function index() {
 
         $apartments = Apartment::all()->shuffle();
-        //   seleziono dal db solo gli apt sponsorizzati
-        $apt_sponsor = Apartment::all()->shuffle();
 
+        $all_sponsorships=[];
+        foreach ($apartments as $apartment) {
+            array_push($all_sponsorships, DB::table('apartment_sponsorship')->where('apartment_id', $apartment->id)->get());
+        }
 
-        return view('public.home', ["apartments"=>$apartments, 'apts_sponsor'=>$apt_sponsor]);
+        $active_sponsorships = [];
+        // scorro la lista di tutte le sponsorizzazioni per gli appartamenti dell'utente e mi ricavo solo quelle attive
+        for ($i=0; $i < count($all_sponsorships); $i++) {
+            for ($j=0; $j < count($all_sponsorships[$i]); $j++) {
+                // leggo la data di inizio sponsorizzazione
+                $start_date = $all_sponsorships[$i][$j]->{'start_date'};
+                // la trasformo in un oggettoo Carbon
+                $start_date = Carbon::create($start_date);
+                // leggo il tipo della sponsorizzazione
+                $type = $all_sponsorships[$i][$j]->{'sponsorship_id'};
+
+                // calcolo la durata della sponsorizzazione in 'numero di giorni'
+                $num_of_days = ($type == 3 ? 6 : $type);
+
+                // calcolare la end date sommando alla start_date la durata del tipo di sponsorizzazione
+                $end_date = $start_date->addDay($num_of_days);
+
+                // verifico se la sponsorizzazione è ancora attiva
+                if ($end_date > now('Europe/Rome')) {
+                    // la sponsorizzazione è attiva, la metto in elenco nell'array, pushando l'id dell'appartamento
+                    array_push($active_sponsorships, $all_sponsorships[$i][$j]->{'apartment_id'});
+                }
+            }
+        }
+
+        $apt_sponsor = Apartment::whereIn('id', $active_sponsorships)->get()->shuffle();
+
+        // calcolo del indice per il foreach dello stamapa card apt
+        $apt_num = count($apt_sponsor) >= 3 ? 2 : 5 - count($apt_sponsor);
+        
+        return view('public.home', ["apartments"=>$apartments, 'apts_sponsor'=>$apt_sponsor, 'apt_num' => $apt_num]);
     }
 
     public function show(Apartment $apartment) {
@@ -41,6 +75,7 @@ class HomeController extends Controller
 
         return view('public.show', ["apartment"=>$apartment]);
     }
+
     public function mail(Request $request, Apartment $apartment) {
 
         $request->validate([
@@ -61,6 +96,7 @@ class HomeController extends Controller
 
         return view('public.message_sent', ["apartment"=>$apartment, 'new_message' => $new_message]);
     }
+    
     public function go_back() {
 
         return back();
--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -11,37 +11,37 @@
         <div class="bd-example margin-bottom-xl">
             <div class="row row-cols-1 row-cols-md-3">
             @foreach($apts_sponsor as $apt_sponsor)
-              @break($loop->index > 2)
-                <a class="apt-card mb-3" href="{{ route('public.show', [$apt_sponsor->id]) }}">
-                    <div class="sponsor card h-100">
-                        <div class="img-container">
-                            <img class="img-apt-card card-body card-img-top" src="{{$apt_sponsor->info->image ? asset('storage/' . $apt_sponsor->info->image) : asset('storage/uploads/no_apt_img.png')}}" alt="{{$apt_sponsor->title}}">
-                        </div>
+                @break($loop->index > 2)
+                    <a class="apt-card mb-3" href="{{ route('public.show', [$apt_sponsor->id]) }}">
+                        <div class="sponsor card h-100">
+                            <div class="img-container">
+                                <img class="img-apt-card card-body card-img-top" src="{{$apt_sponsor->info->image ? asset('storage/' . $apt_sponsor->info->image) : asset('storage/uploads/no_apt_img.png')}}" alt="{{$apt_sponsor->title}}">
+                            </div>
 
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $apt_sponsor->city }}</h5>
-                            <p class="card-text"><strong>{{ $apt_sponsor->title }}</strong></p>
-                            <span class="neon-sponsor h5"><strong>SPONSORIZZATO</strong></span>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $apt_sponsor->city }}</h5>
+                                <p class="card-text"><strong>{{ $apt_sponsor->title }}</strong></p>
+                                <span class="neon-sponsor h5"><strong>SPONSORIZZATO</strong></span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-              @endforeach
+                    </a>
+                @endforeach
 
-              @foreach($apartments as $apartment)
-              @break($loop->index > 2)
-                <a class="apt-card mb-3" href="{{ route('public.show', [$apartment->id]) }}">
-                    <div class="card h-100">
-                        <div class="img-container">
-                            <img class="img-apt-card card-body card-img-top" src="{{$apartment->info->image ? asset('storage/' . $apartment->info->image) : asset('storage/')}}" alt="{{$apartment->title}}">
-                        </div>
+                @foreach($apartments as $apartment)
+                @break($loop->index > $apt_num)
+                    <a class="apt-card mb-3" href="{{ route('public.show', [$apartment->id]) }}">
+                        <div class="card h-100">
+                            <div class="img-container">
+                                <img class="img-apt-card card-body card-img-top" src="{{$apartment->info->image ? asset('storage/' . $apartment->info->image) : asset('storage/')}}" alt="{{$apartment->title}}">
+                            </div>
 
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $apartment->city }}</h5>
-                            <p class="card-text">{{ $apartment->title }}</p>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $apartment->city }}</h5>
+                                <p class="card-text">{{ $apartment->title }}</p>
+                            </div>
                         </div>
-                    </div>
-                </a>
-              @endforeach
+                    </a>
+                @endforeach
             </div>
         </div>
     </div>
