24ccee2478bcb599db43b32f068c5541d751f850
Gi@da <giadavolpi8@gmail.com>
2020-03-26 12:57:53 +0100

    popolata tabella apartments con dati reali

--- database/seeds/ApartmentsTableSeeder.php
+++ database/seeds/ApartmentsTableSeeder.php
@@ -15,13 +15,21 @@ class ApartmentsTableSeeder extends Seeder
      //
 
 
-     // numero di appartamenti "fake" da creare
-     public $num_of_Apartments = 30;
 
     public function run()
     {
-        // chiamo la funzione "factory" per num_of_Apartments volte, per riempiere le righe della tabella
-        factory(App\Apartment::class, $this->num_of_Apartments)->create();
+        // leggo il file 'apartment.php' dove c'e un array con chiave 'apt_table'
+        $apartments = config('apartment.apt_table');
+        // ciclo i dati letti dal file
+        foreach ($apartments as $apartment) {
+            // creo un nuovo oggetto di tipo Apartment
+            $new_apt = new Apartment();
 
+            // ci assegno i dati che ho messo in '$apartment', letti dal file apartment.php, cioè l'array con chiave 'apt_table'
+            $new_apt->fill($apartment);
+
+            // salvo l'oggetto nel DB
+            $new_apt->save();
+        }
     }
 }
